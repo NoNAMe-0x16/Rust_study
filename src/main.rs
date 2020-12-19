7e493cbf895edb64308b96a88eddb17ebd260ca7
Gyutae <einokt@naver.com>
2020-12-19 23:55:09 +0900

    practice rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut N = String::new();
+    io::stdin().read_line(&mut N)
+        .expect("error");
+    let N: u32 = N.trim().parse()
+        .expect("wrong type!");
+
+    for i in 1..N + 1 {
+        let mut k = i;
+        while k > 0 {
+            print!("*");
+            k -= 1;
+        }
+        println!("");
+    }
+    for i in (1 .. N).rev() {
+        let mut k = i;
+        while k > 0 {
+            print!("*");
+            k -= 1;
+        }
+        println!("");
+    }
 }
