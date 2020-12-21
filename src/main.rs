049c30a5c66844f9cc19030b8c02ec1bc296df40
Gyutae <einokt@naver.com>
2020-12-21 23:57:53 +0900

    practice rust

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,23 @@ fn main() {
     let mut N = String::new();
     io::stdin().read_line(&mut N)
         .expect("error");
-    let N: u32 = N.trim().parse()
-        .expect("wrong type!");
+    let mut num:Vec<u32> = N.trim().split(" ")
+        .map(|x| {
+            x.parse::<u32>()
+                .expect("Not an integer!")
+        })
+        .collect();
 
-    for i in 1..N + 1 {
-        let mut k = i;
-        while k > 0 {
-            print!("*");
-            k -= 1;
+    let mut sum:u32 = 0;
+    while num[0] != 0 {
+        let mut tmp:u32 = num[1];
+        let src:u32 = num[0] % 10;
+        while tmp != 0 {
+            sum = sum + (src * (tmp % 10));
+            tmp = tmp / 10;
         }
-        println!("");
+        num[0] = num[0] / 10;
     }
-    for i in (1 .. N).rev() {
-        let mut k = i;
-        while k > 0 {
-            print!("*");
-            k -= 1;
-        }
-        println!("");
-    }
+
+    print!("{}", sum);
 }
